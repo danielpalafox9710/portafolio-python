fca0c5f0b6fcf71611c4901136065491ed7835c2
Jonathan Daniel Palafox Rodriguez <daniel.palafox.9710@gmail.com>
2026-01-07 14:17:36 -0600

    ~

---
+++ prueb_excepciones2.py
@@ -0,0 +1,21 @@
+def divide():
+
+	try:
+
+		op1=float(input("Introduce el primer número"))
+		op2=float(input("Introduce el segundo número"))
+
+		print("La división es: " +str(op1/op2))
+	except ValueError:
+		print("El valor introducido es erróneo")
+
+
+	except ZeroDivisionError:
+		print("No se puede dividir entre 0")
+
+	except:
+		print("No se encontró el error, intentar de nuevo...")
+
+	print("Calculo finalizado")
+
+divide()
